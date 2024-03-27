fd2f4623eee9683185e52e81543a7aca3ba93b21
Ranajit Roy <ranajit.roy@colorado.edu>
2024-03-27 02:06:48 -0600

    header preliminary template added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders Options', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Option Link-1/i);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
