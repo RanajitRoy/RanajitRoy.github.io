b8b24f32ff0584202dfa5faa6d966519a85cdb8d
Ranajit Roy <ranajit.roy@colorado.edu>
2024-03-29 02:05:39 -0600

    responsive design changes

--- src/body/sections/Home.tsx
+++ src/body/sections/Home.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./Home.css";
 import style_pic from "../../assets/me_style.png"
-import backend_img from "../../assets/backend.png"
-import frontend_img from "../../assets/wireframe.png"
-import db_img from "../../assets/database-storage.png"
 
 
 function Home() {
     return (
         <div className="Section Home" id="home">
             <img src={style_pic} alt="style_pic" />
-            <div className="HomeFlyers flyer1">
+            {/* <div className="HomeFlyers flyer1">
                 <img src={backend_img} alt="backend"/>
                 <div className="wavy-line wavy-line1" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/wavy.svg'})`}} ></div>
                 <div className="wavy-line wavy-line2" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/wavy.svg'})`}} ></div>
@@ -27,7 +24,7 @@ function Home() {
                 <div className="wavy-line wavy-line1" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/wavy.svg'})`}} ></div>
                 <div className="wavy-line wavy-line2" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/wavy.svg'})`}} ></div>
                 <div className="wavy-line wavy-line3" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/wavy.svg'})`}} ></div>
-            </div>
+            </div> */}
             <div className="HomeTextBox">
                 <p>Welcome to My Portfolio!</p>
             </div>
